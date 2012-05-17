55315656abc8c8b3b97255e4e81398908b7c335d
Keith Branton <kbranton@slipstonemethod.com>
2012-05-17 09:29:25 -0600

    Html escape source code

--- src/main/java/com/olabini/jescov/CoverageData.java
+++ src/main/java/com/olabini/jescov/CoverageData.java
@@ -1,91 +1,95 @@
 package com.olabini.jescov;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
-import java.util.Collection;
 import java.util.Set;
-import java.util.HashSet;
-import java.util.ArrayList;
+import java.util.TreeMap;
 
 public class CoverageData {
-    private final Map<String, FileCoverage> fileCoverage;
+	private final Map<String, FileCoverage> fileCoverage;
 
-    public CoverageData(List<FileCoverage> fileCoverage) {
-        this.fileCoverage = new TreeMap<String, FileCoverage>();
-        for(FileCoverage fc : fileCoverage) {
-            this.fileCoverage.put(fc.getFilename(), fc);
-        }
-    }
+	public CoverageData(final List<FileCoverage> fileCoverage) {
+		this.fileCoverage = new TreeMap<String, FileCoverage>();
+		for (final FileCoverage fc : fileCoverage) {
+			this.fileCoverage.put(fc.getFilename(), fc);
+		}
+	}
 
-    public FileCoverage getFileCoverageFor(String filename) {
-        return fileCoverage.get(filename);
-    }
+	public Map<String, FileCoverage> getFileCoverage() {
+		return fileCoverage;
+	}
 
-    public Collection<String> getFileNames() {
-        return fileCoverage.keySet();
-    }
+	public FileCoverage getFileCoverageFor(final String filename) {
+		return fileCoverage.get(filename);
+	}
 
-    public int getLinesValid() {
-        int sum = 0;
-        for(FileCoverage fc : fileCoverage.values()) {
-            sum += fc.getLinesValid();
-        }
-        return sum;
-    }
+	public Collection<String> getFileNames() {
+		return fileCoverage.keySet();
+	}
 
-    public int getBranchesValid() {
-        int sum = 0;
-        for(FileCoverage fc : fileCoverage.values()) {
-            sum += fc.getBranchesValid();
-        }
-        return sum;
-    }
+	public int getLinesValid() {
+		int sum = 0;
+		for (final FileCoverage fc : fileCoverage.values()) {
+			sum += fc.getLinesValid();
+		}
+		return sum;
+	}
 
-    public int getLinesCovered() {
-        int sum = 0;
-        for(FileCoverage fc : fileCoverage.values()) {
-            sum += fc.getLinesCovered();
-        }
-        return sum;
-    }
+	public int getBranchesValid() {
+		int sum = 0;
+		for (final FileCoverage fc : fileCoverage.values()) {
+			sum += fc.getBranchesValid();
+		}
+		return sum;
+	}
 
-    public int getBranchesCovered() {
-        int sum = 0;
-        for(FileCoverage fc : fileCoverage.values()) {
-            sum += fc.getBranchesCovered();
-        }
-        return sum;
-    }
+	public int getLinesCovered() {
+		int sum = 0;
+		for (final FileCoverage fc : fileCoverage.values()) {
+			sum += fc.getLinesCovered();
+		}
+		return sum;
+	}
 
-    public double getLineRate() {
-        return getLinesCovered() / (double)getLinesValid();
-    }
+	public int getBranchesCovered() {
+		int sum = 0;
+		for (final FileCoverage fc : fileCoverage.values()) {
+			sum += fc.getBranchesCovered();
+		}
+		return sum;
+	}
 
-    public double getBranchRate() {
-        return getBranchesCovered() / (double)getBranchesValid();
-    }
+	public double getLineRate() {
+		return getLinesCovered() / (double) getLinesValid();
+	}
 
-    public CoverageData plus(CoverageData other) {
-        List<FileCoverage> fcs = new ArrayList<FileCoverage>();
-        Set<String> files = new HashSet<String>();
-        files.addAll(this.getFileNames());
-        files.addAll(other.getFileNames());
-        for(String file : files) {
-            FileCoverage left = this.getFileCoverageFor(file);
-            FileCoverage right = other.getFileCoverageFor(file);
+	public double getBranchRate() {
+		return getBranchesCovered() / (double) getBranchesValid();
+	}
 
-            if(left == null || right == null) {
-                if(left == null) {
-                    fcs.add(right);
-                } else {
-                    fcs.add(left);
-                }
-            } else {
-                fcs.add(left.plus(right));
-            }
-        }
+	public CoverageData plus(final CoverageData other) {
+		final List<FileCoverage> fcs = new ArrayList<FileCoverage>();
+		final Set<String> files = new HashSet<String>();
+		files.addAll(this.getFileNames());
+		files.addAll(other.getFileNames());
+		for (final String file : files) {
+			final FileCoverage left = this.getFileCoverageFor(file);
+			final FileCoverage right = other.getFileCoverageFor(file);
 
-        return new CoverageData(fcs);
-    }
+			if (left == null || right == null) {
+				if (left == null) {
+					fcs.add(right);
+				} else {
+					fcs.add(left);
+				}
+			} else {
+				fcs.add(left.plus(right));
+			}
+		}
+
+		return new CoverageData(fcs);
+	}
 }
